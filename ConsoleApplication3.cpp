1b7fed5d7b6cc8e182b7722234d8befda29d69b4
hw2012060050024 <562421007@qq.com>
2013-09-06 18:47:32 +0800

    hello world

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication3.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ cout<<"hello world"<<endl;
+  cout<<"黄文"<<endl;
+
+ return 0;
+
+}
